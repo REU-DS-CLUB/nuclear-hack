a6b63561b392c32dc463a573bfb2e24c3fbeeaa0
PaLashilo <93659071+PaLashilo@users.noreply.github.com>
2024-04-16 17:09:23 +0300

    test logging

--- wandb_logging.py
+++ wandb_logging.py
@@ -4,13 +4,15 @@ import json
 with open('config.json', 'r') as file:
     config = json.load(file)
 
-def init(exp_tags):
-
+def init_exp(exp_tags=[]):
     if config['logging']:
         with open('wandb_secret.txt', 'r') as file:
             api_key = file.read()
-        wandb.login(api_key)
+        wandb.login(key=api_key)
         wandb.init(project="nuclear-hack", tags=exp_tags)
 
-def log_params():
-    ...
+
+def log_params(params={}):
+    if config['logging']:
+        wandb.log(params)
+        wandb.finish()
