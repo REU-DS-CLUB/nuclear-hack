0e4043254df90bd78f73f5d124e68586f9aa82b3
Александр <lild1tz2023@gmail.com>
2024-04-18 17:53:27 +0300

    add gigacode and metro dir

---
+++ gigachat/gigachat_utils.py
@@ -0,0 +1,99 @@
+import requests
+import json
+import uuid
+
+def get_token(auth_token, scope='GIGACHAT_API_PERS'):
+    """
+      Выполняет POST-запрос к эндпоинту, который выдает токен.
+
+      Параметры:
+      - auth_token (str): токен авторизации, необходимый для запроса.
+      - область (str): область действия запроса API. По умолчанию — «GIGACHAT_API_PERS».
+
+      Возвращает:
+      - ответ API, где токен и срок его "годности".
+      """
+    # Создадим идентификатор UUID (36 знаков)
+    rq_uid = str(uuid.uuid4())
+
+    # API URL
+    url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+
+    # Заголовки
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'RqUID': rq_uid,
+        'Authorization': f'Basic {auth_token}'
+    }
+
+    # Тело запроса
+    payload = {
+        'scope': scope
+    }
+
+    try:
+        # Делаем POST запрос с отключенной SSL верификацией
+        # (можно скачать сертификаты Минцифры, тогда отключать проверку не надо)
+        response = requests.post(url, headers=headers, data=payload, verify=False)
+        return response
+    except requests.RequestException as e:
+        print(f"Ошибка: {str(e)}")
+        return -1
+
+
+def get_chat_station(auth_key, user_message):
+    """
+    Отправляет POST-запрос к API чата для получения ответа от модели GigaChat.
+
+    Параметры:
+    - auth_token (str): Токен для авторизации в API.
+    - user_message (str): Сообщение от пользователя, для которого нужно получить ответ.
+
+    Возвращает:
+    - str: Ответ от API в виде текстовой строки.
+    """
+    # URL API, к которому мы обращаемся
+
+    response = get_token(auth_token=auth_key)
+    if response != -1:
+        giga_token = response.json()['access_token']
+    else:
+        return "Проблема при осуществулении доступа к GigaChat Token"
+
+    url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
+
+    # Подготовка данных запроса в формате JSON
+    payload = json.dumps({
+        "model": "GigaChat",  # Используемая модель
+        "messages": [
+            {
+                "role": "user",  # Роль отправителя (пользователь)
+                "content": user_message  # Содержание сообщения
+            }
+        ],
+        "temperature": 1,  # Температура генерации
+        "top_p": 0.1,  # Параметр top_p для контроля разнообразия ответов
+        "n": 1,  # Количество возвращаемых ответов
+        "stream": False,  # Потоковая ли передача ответов
+        "max_tokens": 512,  # Максимальное количество токенов в ответе
+        "repetition_penalty": 1,  # Штраф за повторения
+        "update_interval": 0  # Интервал обновления (для потоковой передачи)
+    })
+
+    # Заголовки запроса
+    headers = {
+        'Content-Type': 'application/json',  # Тип содержимого - JSON
+        'Accept': 'application/json',  # Принимаем ответ в формате JSON
+        'Authorization': f'Bearer {giga_token}'  # Токен авторизации
+    }
+
+    # Выполнение POST-запроса и возвращение ответа
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+        answer = response.json()
+        return json.loads(answer['choices'][0]['message']['content'])["station"]
+    except requests.RequestException as e:
+        # Обработка исключения в случае ошибки запроса
+        print(f"Произошла ошибка: {str(e)}")
+        return -1
